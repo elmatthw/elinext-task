8089028de243921c6ec4f8c40a451a400c65d25a
Elena Kulinich <elmatthw8@gmail.com>
2019-09-11 23:47:34 +0300

    fixed request to upload data on server; created insert query to mongo

--- routes/data.js
+++ routes/data.js
@@ -6,24 +6,30 @@ async function routes(fastify, options) {
     const database = fastify.mongo.db('db')
     const collection = database.collection('archives')
     fastify.register(require('fastify-multipart'))
-    fastify.addContentTypeParser('multipart/form-data', function (req, done) {
-        formDataParser(req, function (err, body) {
-          done(err, body)
-        })
-      })
+    
     fastify.post('/save', async (request, reply) => {
-        const mp = request.multipart(handler, function (err) {
-            console.log('upload completed')
-            reply.code(200).send()
-          })
-          mp.on('field', function (key, value) {
-            console.log('form-data', key, value)
-          })
+      const mp = request.multipart(handler, function (err) {
         
-          function handler (field, file, filename, encoding, mimetype) {
-            pump(file, fs.createWriteStream('./storage/' + filename))
-          }
+        reply.code(200).send()
+      })
+        mp.on('field', function (key, value) {
+          console.log('form-data', key, value)
+        })
+      
+        async function handler (field, file, filename, encoding, mimetype) {
+          await saveArchive({title: filename, description: 'nice', expire: new Date("<2020-12-22T08:00:00Z>")})
+          pump(file, fs.createWriteStream('./storage/' + filename))
+        }
     })
+
+    async function saveArchive(json){
+        return await collection.insertOne(json, function(err, res) {
+          if (err)
+            throw err;
+          console.log('one document inserted')
+          console.log(res);
+        });
+    }
 
     fastify.get('/', async(request, reply) => {
         reply.view('/index.ejs');
