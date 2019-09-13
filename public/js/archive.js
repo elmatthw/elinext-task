fa66c1d680f66a61f5789bc054a367a1302c6bdd
elmatthw <elmatthw8@gmail.com>
2019-09-13 13:40:12 +0300

    refactoring: moved scripts from ejs to separate js file

---
+++ public/js/archive.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    $.urlParam = function(name) {
+        var results = new RegExp('[?&]' + name + '=([^&#]*)').exec(window.location.href);
+        if (!results) {
+            return 0;
+        }
+        return results[1] || 0;
+    };
+
+
+    $.ajax({
+        type: 'GET',
+        url: 'archive/?id=' + $.urlParam('id'),
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+        success: function(data){
+            $.ajax({
+                type: 'GET',
+                url: 'archive',
+                dataType: "json",
+                contentType: "application/json; charset=UTF-8"
+            })
+            $('div').append(data.title + '<br>' + data.description);
+        
+        }
+    })
+})
---
+++ public/js/archives.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+
+    $.ajax({    
+        type: 'GET',
+        url: '/archives',
+        cache: false,
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+        complete: function(data){
+            $.ajax({
+                type: 'GET',
+                url: '/all-archives',
+                cache: false,
+                dataType: "json",
+                contentType: "application/json; charset=UTF-8",
+                success: function(data){
+                    $('#demo').pagination({
+                        dataSource: data.archives,
+                        pageSize: 5,
+                        showPageNumbers: false,
+                        showNavigator: true,
+                        callback: function(data, pagination) {
+                            $('div.data-container').empty();
+                            data.forEach(element => {
+                                $('div.data-container').append('<div id="'+element._id + '">' + element.title + '<br>' + element.description + '</div>');
+                                $('div.data-container').append('<br><button id="'+element._id + '">More</button><br><br>')
+                            });
+                            $('button').on('click', function(event){
+                                event.preventDefault();
+                                event.stopPropagation();
+
+                                let id = this.getAttribute('id');
+                                window.location='archive?id=' + id;
+                            })
+                            /* var html = template(data);
+                            dataContainer.html(html); */
+                        }
+                    })
+                }
+            })
+        },
+        error: function(data){
+
+        }
+    })
+})
